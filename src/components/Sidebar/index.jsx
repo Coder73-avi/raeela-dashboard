fdf5c028d13bda087be43d1dc3e99551b6b96c65
Coder73-avi <aavishek60@gmail.com>
2022-06-29 17:18:44 +0545

    add some style"

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -5,13 +5,15 @@ import { MdDashboardCustomize } from "react-icons/md";
 import { FcSettings } from "react-icons/fc";
 import { BsNewspaper } from "react-icons/bs";
 import { RiCalendarEventFill } from "react-icons/ri";
-import { AiOutlineUsergroupAdd, AiOutlineMenuUnfold } from "react-icons/ai";
+import { AiOutlineUsergroupAdd } from "react-icons/ai";
+import { BiShekel, BiMessageDetail } from "react-icons/bi";
 import css from "./style.module.css";
 
 const Sidebar = () => {
   const DashNav = [
     { name: "Dashboard", path: "/", icons: <MdDashboardCustomize /> },
     { name: "Client Info", path: "/clients", icons: <AiOutlineUsergroupAdd /> },
+    { name: "Messages", path: "/clients", icons: <BiMessageDetail /> },
     { name: "Pages", path: "/pages", icons: <BsNewspaper /> },
     { name: "Events ", path: "/events", icons: <RiCalendarEventFill /> },
     { name: "Settings ", path: "/settings", icons: <FcSettings /> },
@@ -21,16 +23,15 @@ const Sidebar = () => {
     <>
       <section className={` ${css.sidebar}`}>
         <div
-          className={`p-4 flex flex-row items-center justify-center gap-6  ${css.heading}`}
+          className={` flex flex-row items-center justify-center gap-6  ${css.heading}`}
         >
-          <div className={`text-xl ${css.dashIcon}`}>
-            <AiOutlineMenuUnfold />
+          <div className={`text-4xl ${css.dashIcon}`}>
+            <BiShekel />
           </div>
-          <h1 className="text-center text-xl font-medium uppercase">
-            Raeela Admin
+          <h1 className="text-center text-xl font-bold uppercase">
+            Raeela <span style={{ color: "var(--color-danger)" }}>Admin</span>
           </h1>
         </div>
-        <hr />
 
         {/* navigation */}
         <nav className={`mt-8 ${css.navigation}`}>
@@ -43,7 +44,7 @@ const Sidebar = () => {
                       window.location.pathname === path && css.active
                     } ${css.navLink}`}
                   >
-                    {icons}
+                    <span className="text-xl">{icons}</span>
                     {name}
                   </li>
                 </NavLink>
@@ -54,10 +55,23 @@ const Sidebar = () => {
       </section>
 
       <section className="sideContain">
-        <div className={`text-xl `}>
-          <AiOutlineMenuUnfold />
+        <div className="grid grid-cols-5 gap-4">
+          <div className="col-span-3">
+            <h1 className={`text-2xl font-bold`}>Dashboard</h1>
+            <div className={`my-4 ${css.date}`}>
+              <input type="date" />
+            </div>
+
+            <div className={`grid grid-cols-2 gap-6`}>
+              <div className="bg-white shadow-black rounded-xl h-44"></div>
+              <div className="bg-white shadow-black rounded-xl h-44"></div>
+            </div>
+
+            <div className={`${css.recent__activities}`}></div>
+          </div>
+
+          <div className="col-span-2">side</div>
         </div>
-        <h1>Dashboard</h1>
       </section>
     </>
   );
